381e7b805913f76ce50d180b9e96b107201291cc
lilisep <lilianasepulveda13@gmail.com>
2024-08-11 17:18:23 -0500

    checklist app in progress

---
+++ src/TodoSearch.js
@@ -0,0 +1,17 @@
+import "./TodoSearch.css";
+import React from "react";
+
+function TodoSearch({searchValue, setSearchValue}) {
+
+  return (
+    <input
+      placeholder="Cortar cebolla"
+      className="TodoSearch"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }} />
+  );
+}
+
+export { TodoSearch };
